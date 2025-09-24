212351fae4a9d9588572dc811d0c5fce60970bab
Kayjoon <gdjoon73@gmail.com>
2025-09-25 01:35:05 +0900

    test

---
+++ functions/auth.js
@@ -0,0 +1,25 @@
+const fetch = require("node-fetch");
+
+exports.handler = async (event, context) => {
+  const token = event.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return { statusCode: 401, body: "Unauthorized" };
+  }
+
+  // Auth0 토큰 검증
+  const response = await fetch(`https://YOUR_DOMAIN.auth0.com/userinfo`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  if (!response.ok) {
+    return { statusCode: 401, body: "Invalid token" };
+  }
+
+  const user = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ user }),
+  };
+};
